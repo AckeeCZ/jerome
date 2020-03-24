ff3e9997bb6a20771c70303c5eaff38d0d949840
Marek Janča <marek.janca@ackee.cz>
2020-03-24 11:40:48 +0100

    ✨ Reuses intl cache for new instances

--- src/HOC/translatableFactory.tsx
+++ src/HOC/translatableFactory.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { RawIntlProvider, createIntl, createIntlCache, IntlShape } from 'react-intl';
+import { RawIntlProvider, createIntl, createIntlCache, IntlShape, IntlCache } from 'react-intl';
 import getDisplayName from 'react-display-name';
 
 import { LocaleData, LocaleState, State } from '../types';
@@ -17,11 +17,31 @@ export interface WrappedProps extends LocaleState {
 interface WrappedState {
     locale: string;
     intl: IntlShape;
+    cache: IntlCache;
+}
+
+function shouldRecreateIntl(props: WrappedProps, state: WrappedState): boolean {
+    return !state || props.locale !== state.locale;
 }
 
 const translatableFactory = (intlLocaleData: LocaleData): any => {
+    function prepareConfig(props: WrappedProps) {
+        return {
+            locale: props.locale,
+            messages: intlLocaleData[props.locale],
+        };
+    }
+
     return (TranslatableComponent: React.ComponentClass<WrappedProps>) => {
         class Translatable extends Component<WrappedProps, WrappedState> {
+            private cache: IntlCache = createIntlCache();
+
+            public state: WrappedState = {
+                locale: this.props.locale,
+                intl: createIntl(prepareConfig(this.props), this.cache),
+                cache: this.cache,
+            };
+
             static displayName = `Translatable(${getDisplayName(TranslatableComponent)})`;
 
             static propTypes = {
@@ -31,16 +51,9 @@ const translatableFactory = (intlLocaleData: LocaleData): any => {
             };
 
             static getDerivedStateFromProps(props: WrappedProps, prevState: WrappedState) {
-                if (!prevState || props.locale !== prevState.locale) {
-                    const intlConfig = {
-                        locale: props.locale,
-                        messages: intlLocaleData[props.locale],
-                    };
-
-                    const cache = createIntlCache();
-                    const intl = createIntl(intlConfig, cache);
+                if (shouldRecreateIntl(props, prevState)) {
                     return {
-                        intl,
+                        intl: createIntl(prepareConfig(props), prevState.cache),
                         locale: props.locale,
                     };
                 }
@@ -54,7 +67,7 @@ const translatableFactory = (intlLocaleData: LocaleData): any => {
             }
 
             componentDidUpdate(_: WrappedProps, prevState: WrappedState) {
-                if (this.state.intl !== prevState.intl) {
+                if (this.state.locale !== prevState.locale) {
                     this.props.destroyIntlProvider();
                     this.props.createIntlProvider({
                         intl: this.state.intl,
