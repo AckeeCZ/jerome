90543e9fd719261227ca184db70a357051bcd9cd
Lukas Horak <horaklukas@centrum.cz>
2020-05-04 13:56:09 +0200

    ✅Fix imports test

--- src/__tests__/imports.test.ts
+++ src/__tests__/imports.test.ts
@@ -3,8 +3,8 @@ import 'babel-polyfill';
 import translatableFactory from '../../lib/HOC/translatableFactory';
 import translatableWithAntdFactory from '../../lib/HOC/translatableWithAntdFactory';
 
-import * as standardJeromeImport from '../../../ackee-jerome';
-import * as antdJeromeImport from '../../../ackee-jerome/antd';
+import * as standardJeromeImport from '../..';
+import * as antdJeromeImport from '../../antd';
 
 describe('Imports', () => {
     it('HOC factory available from default import is the one without antd', () => {
